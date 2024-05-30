9e623699876b4b18121e73f178a6b096c7624042
Jeciane Silva <jeciane_amorim@hotmail.com>
2024-05-30 14:45:19 -0300

    feat: update colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
 import "./styles/global.css";
+import { Header } from "./components/Header/header";
+import { Footer } from "./components/Footer/footer";
+
+import HeroImg from "./assets/images/bg-hero.png";
 
 function App() {
   return (
-    <>
-      <h1 className="font-bold bg-yellow-400">Hello world</h1>
-      <span className="font-bold font-cursive">Complete seu pedido</span>
-    </>
+    <div className="flex flex-col w-screen h-screen">
+      <Header></Header>
+      <div className="flex">
+        <div className="flex flex-col max-w-[500px] h-screen p-8">
+          <button
+            disabled
+            className="bg-sky-500 px-4 py-2 rounded-md font-medium mt-4 enabled:hover:bg-sky-600 disabled:opacity-60 disabled:cursor-not-allowed"
+          >
+            Sign in
+          </button>
+        </div>
+      </div>
+      <div className="bottom-0 h-full">
+        <img src={HeroImg} />
+      </div>
+      <Footer></Footer>
+    </div>
+
   )
 }
 
---
+++ src/components/Header/header.tsx
@@ -0,0 +1,9 @@
+
+export function Header() {
+  return (
+    <header className="min-h-[70px] bg-red-600 flex items-center">
+      <img src="../assets/icons/logo-white.svg" alt="icon logo" className="px-8" />
+      <h3 className="font-bold uppercase text-white">Intergaláctico Food</h3>
+    </header>
+  );
+}

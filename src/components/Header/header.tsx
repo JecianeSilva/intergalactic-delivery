37d7914809815925ca3d21fd13f12601f8c5ea84
Jeciane Silva <jeciane_amorim@hotmail.com>
2024-05-30 15:16:17 -0300

    fix: update name food for delivery

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -1,11 +1,11 @@
 import { Planet } from "phosphor-react";
 export function Header() {
   return (
-    <header className="min-h-[70px] bg-blue-600 flex items-center">
+    <header className="min-h-[70px] bg-blue-600 flex items-center px-4">
       <div className="flex max-w-7xl w-full mx-auto justify-between">
         <div className="flex">
           <Planet size={24} color="#FFFFFF" weight="fill" className="mr-4" />
-          <h3 className="flex flex-1 font-bold uppercase text-white letter-lg">Intergaláctico Food</h3>
+          <h3 className="flex flex-1 font-bold uppercase text-white letter-lg">Delivery Intergaláctico</h3>
         </div>
       </div>
     </header>

e44ef7335a8c114fbb29f44e6599082bf1338eba
Jeciane Silva <jeciane_amorim@hotmail.com>
2024-05-30 11:51:38 -0300

    feat: init project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./styles/global.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="font-bold bg-yellow-400">Hello world</h1>
+      <span className="font-bold font-cursive">Complete seu pedido</span>
+    </>
+  )
+}
+
+export default App

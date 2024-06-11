2b01edfea67018279378cf45f4e5e4ac81043b0d
Jeciane Silva <jeciane_amorim@hotmail.com>
2024-06-10 21:37:48 -0300

    feat: created cart

--- src/pages/Cart/cart.tsx
+++ src/pages/Cart/cart.tsx
@@ -1,24 +1,72 @@
-import { ProductCard, ProductProps } from "../../components/ProductCard/ProductCard";
+import { Button } from "../../components/Button/Button";
 
-function Cart() {
-  let productsFilter: ProductProps[] = [];
+import { useCart } from "../../contexts/Cart/CartContext";
+
+import { formatPrice } from "../../utils/mask";
+import { Modal } from "../../components/Modal/Modal";
+import { WarningCircle } from "phosphor-react";
+import { useNavigate } from "react-router-dom";
+import ProductCardItems from "../../components/ProductCardItems/ProductCardItems";
+
+export default function Card() {
+  const {
+    cart,
+  } = useCart();
+
+  const navigate = useNavigate();
+  const total = cart.reduce(
+    (acc, product) => acc + product.price * product.quantity,
+    0
+  );
 
   return (
-    <div className="flex flex-col w-full h-full">
-      <div className="flex max-w-6xl w-full px-4 mx-auto pb-[80px] max-md:flex-col max-md:gap-8">
-        <div className="flex w-full flex-col justify-between mb-14 max-md:flex-col max-md:items-start">
-          <div className="w-full grid mt-10 gap-8 grid-cols-grid-responsive">
-            {productsFilter.map((product) => (
-              <ProductCard
-                key={product.id}
-                product={product}
+    <form className="flex gap-8 max-w-6xl min-h-fit w-full mx-auto">
+      <div className="w-full mx-auto py-[80px] px-8">
+        {cart.length === 0 ? (
+          <>
+            <div className="bg-gray-100 p-10 w-[448px] rounded-tl-md rounded-tr-[44px] rounded-br-md rounded-bl-[44px] flex justify-center flex-col gap-6 max-[600px]:w-full max-[475px]:p-4">
+              <WarningCircle size={72} className="mx-auto max-[475px]:h-[50px]" />
+              <strong className="font-cursive font-extrabold text-[1.5rem] text-blue-950 text-center max-[475px]:text-[1.2rem]">
+                Adicione alguns dos nossos produtos no carrinho para finalizar o
+                pedido.
+              </strong>
+            </div>
+            <Button label="Voltar" type="button" onClick={() => navigate('/')} className="w-[50%] mx-auto mt-8 mb-20" />
+          </>
+        ) : (
+          <div className="">
+            <Modal title="">
+              <div
+                className={"w-full overflow-y-scroll"}
+              >
+                {cart.map((productInCart) => (
+                  <ProductCardItems key={productInCart.id} product={productInCart} />
+                ))}
+              </div>
+            </Modal>
+            <div className="flex flex-col gap-3 w-full">
+              <span className="font-sans text-sm text-brow-500 flex items-center justify-between">
+                Total de itens
+                <span>R$ {formatPrice(total)}</span>
+              </span>
+            </div>
+            <div className="flex bg-red-200 gap-12 mb-20">
+              <Button
+                type="button"
+                label="Voltar"
+                className="w-[40%] max-[600px]:w-[100%]"
+                onClick={() => navigate('/')}
               />
-            ))}
+              <Button
+                type="submit"
+                label="Confirmar Pedido"
+                className="w-[60%] max-[600px]:w-[100%]"
+              />
+            </div>
           </div>
-        </div>
-      </div>
-    </div >
-  )
+        )
+        }
+      </div >
+    </form >
+  );
 }
-
-export default Cart
--- src/pages/Home/home.tsx
+++ src/pages/Home/home.tsx
@@ -27,18 +27,18 @@ function Home() {
 
   return (
     <div className="flex flex-col w-full h-full">
-      <div className="flex justify-between max-w-6xl w-full px-4 mx-auto pt-[80px] max-md:flex-col max-md:gap-8 max-md:items-center max-md:pt-[40px]">
+      <section className="flex justify-between max-w-6xl w-full px-4 mx-auto pt-[100px] pb-0 max-md:flex-col max-md:gap-8 max-md:items-center max-md:pt-[40px]">
         <div className="flex flex-col mr-8 max-md:mr-0">
           <h1 className="leading-[3.75rem] font-extrabold text-5xl text-slate-950 mb-4 max-[900px]:text-4xl max-md:text-5xl max-md:text-center max-[600px]:text-3xl">
             Sistema de entrega perfeito para qualquer hora do dia
           </h1>
 
-          <p className="font-sans text-lg text-slate-700 mt-4 max-md:text-center max-[600px]:text-sm">
+          <p className="text-lg w-[80%] text-gray-500 my-4 max-md:text-center max-[600px]:text-sm">
             Com o Intergalatic Delivery você recebe suas encomendas onde você quiser, de forma rápida e segura.
           </p>
           <ul
-            className="text-slate-600 
-            grid grid-cols-2 gap-3 mt-12
+            className="text-blue-600 
+            flex flex-col gap-8 mt-12
             max-[600px]:grid-cols-1"
           >
             {listTopicsHome.map((topic) => (
@@ -48,46 +48,47 @@ function Home() {
         </div>
         <img
           src={HeroImg}
-          className="w-[45%] h-auto ml-8  max-md:ml-0  max-md:w-[80%]"
+          className="w-[50%] h-auto mt-12 ml-8  max-md:ml-0  max-md:w-[80%]"
           alt=""
         />
-      </div>
-
-      <div className="flex max-w-6xl w-full px-4 mx-auto pb-[80px] max-md:flex-col max-md:gap-8">
-        <div className="flex w-full flex-col justify-between mb-14 max-md:flex-col max-md:items-start">
-          <h2 className="font-extrabold text-2xl mb-8 mt-12 text-slate-900">
-            Nossos produtos
-          </h2>
-          <div className="flex w-full items-center gap-2 flex-wrap">
-            {listCategoriesProduct.map((typeProduct) => {
-              const isSelectedTag = categoriesProductsSelected?.includes(typeProduct);
-              return (
-                <Tag
-                  key={typeProduct}
-                  variant="outline"
-                  asChild
-                  isSelected={isSelectedTag}
-                >
-                  <div
-                    onClick={() => onUpdateCategoriesProductsSelected(typeProduct)}
+      </section>
+      <section className="bg-slate-50">
+        <div className="flex max-w-6xl w-full px-4 mx-auto pt-[40px] pb-[80px] max-md:flex-col max-md:gap-8">
+          <div className="flex w-full flex-col justify-between mb-14 max-md:flex-col max-md:items-start">
+            <h2 className="font-extrabold text-3xl mb-8 mt-12 text-slate-900">
+              Nossos produtos
+            </h2>
+            <div className="flex w-full items-center gap-2 flex-wrap">
+              {listCategoriesProduct.map((typeProduct) => {
+                const isSelectedTag = categoriesProductsSelected?.includes(typeProduct);
+                return (
+                  <Tag
+                    key={typeProduct}
+                    variant="outline"
+                    asChild
+                    isSelected={isSelectedTag}
                   >
-                    {typeProduct}
-                  </div>
-                </Tag>
-              );
-            })}
-          </div>
+                    <div
+                      onClick={() => onUpdateCategoriesProductsSelected(typeProduct)}
+                    >
+                      {typeProduct}
+                    </div>
+                  </Tag>
+                );
+              })}
+            </div>
 
-          <div className="w-full grid mt-10 gap-8 grid-cols-grid-responsive">
-            {productsFilter.map((product) => (
-              <ProductCard
-                key={product.id}
-                product={product}
-              />
-            ))}
+            <div className="w-full grid mt-10 gap-8 grid-cols-grid-responsive">
+              {productsFilter.map((product) => (
+                <ProductCard
+                  key={product.id}
+                  product={product}
+                />
+              ))}
+            </div>
           </div>
         </div>
-      </div>
+      </section>
     </div >
   )
 }

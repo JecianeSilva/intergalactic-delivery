540ca91b3a419bc2c607b6c09e246e7113477db0
Jeciane Silva <jeciane_amorim@hotmail.com>
2024-06-10 21:36:56 -0300

    style: adjustment footer, bg  and color

--- src/components/Footer/footer.tsx
+++ src/components/Footer/footer.tsx
@@ -1,8 +1,8 @@
 
 export function Footer() {
   return (
-    <footer className="fixed bottom-0 w-screen m-0 min-h-[80px] bg-gray-950 flex items-center justify-center px-4">
-      <span className="copyright text-white text-semibold">Copyright © Jeciane Amorim 2024</span>
+    <footer className="relative bottom-0 left-0 w-full m-0 min-h-12 bg-gray-950 flex items-center justify-center px-4">
+      <span className="copyright text-white text-semibold text-xs">Copyright © <a href="https://jecianesilva.github.io/">Jeciane Amorim</a> 2024</span>
     </footer>
   );
 }

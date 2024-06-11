488ca555af77c75a0fe6144ed871e1652d990eb7
Jeciane Silva <jeciane_amorim@hotmail.com>
2024-06-11 00:08:45 -0300

    feat: adjusment header

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -5,13 +5,13 @@ import { Link } from "react-router-dom";
 export function Header({ isAuthenticated = false }) {
   const { cart } = useCart();
 
-  const totalCoffeesInCart = cart.length;
+  const totalProductsInCart = cart.length;
 
   return (
-    <header className="min-h-[70px] bg-blue-600 flex items-center pl-4">
+    <header className="min-h-[70px] bg-blue-600 flex items-center">
       <div className="flex max-w-7xl w-full mx-auto justify-between px-4">
         <div className="flex items-center">
-          <Planet size={24} color="white" weight="fill" className="mr-4" />
+          <Planet size={22} color="white" weight="fill" className="mr-2" />
           <h3 className="flex flex-1 font-bold uppercase text-white letter-lg">Delivery Intergaláctico</h3>
         </div>
         {isAuthenticated &&
@@ -27,9 +27,9 @@ export function Header({ isAuthenticated = false }) {
               />
             </Link>
 
-            {totalCoffeesInCart > 0 && (
+            {totalProductsInCart > 0 && (
               <span className="absolute -top-1 -right-1 w-5 h-5 bg-red-600 rounded-full flex items-center justify-center text-white font-bold text-xs">
-                {totalCoffeesInCart}
+                {totalProductsInCart}
               </span>
             )}
           </div>}
